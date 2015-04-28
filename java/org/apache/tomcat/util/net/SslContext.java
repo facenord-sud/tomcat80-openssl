686b9233f68a3ee3bcd533979b5f32e47e2aad76
Remy Maucherat <rmaucher@redhat.com>
2015-04-28 12:05:23 +0200

    Cleanup.

--- java/org/apache/tomcat/util/net/SslContext.java
+++ java/org/apache/tomcat/util/net/SslContext.java
@@ -1,8 +1,20 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
+
 package org.apache.tomcat.util.net;
 
 import java.security.KeyManagementException;
@@ -18,18 +30,14 @@ import javax.net.ssl.TrustManager;
 import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
 
-/**
- *
- * @author leo
- */
 public abstract class SslContext {
-    
+
     private static final Log logger = LogFactory.getLog(SslContext.class);
     private static final HashMap<String, SslContext> instances = new HashMap<>();
-    
+
     public static SslContext getInstance(String className, String protocol)
             throws ClassNotFoundException {
-        if(instances.containsKey(className))
+        if (instances.containsKey(className))
             return instances.get(className);
         try {
             Class<?> clazz = Class.forName(className);
@@ -40,14 +48,22 @@ public abstract class SslContext {
         } catch (Exception e) {
             if (logger.isDebugEnabled())
                 logger.debug("Error loading SSL Context " + className, e);
-            throw new ClassNotFoundException("Error loading SSL Context " + className, e);
+            throw new ClassNotFoundException("Error loading SSL Context "
+                    + className, e);
         }
     }
-    
-    public abstract void init(KeyManager[] kms, TrustManager[] tms, SecureRandom sr) throws KeyManagementException;
+
+    public abstract void init(KeyManager[] kms, TrustManager[] tms,
+            SecureRandom sr) throws KeyManagementException;
+
     public abstract SSLSessionContext getServerSessionContext();
+
     public abstract SSLEngine createSSLEngine();
+
     public abstract SSLServerSocketFactory getServerSocketFactory();
+
     public abstract SSLParameters getSupportedSSLParameters();
-    protected abstract void initiateProtocol(String protocol) throws NoSuchAlgorithmException;
+
+    protected abstract void initiateProtocol(String protocol)
+            throws NoSuchAlgorithmException;
 }
--- java/org/apache/tomcat/util/net/jsse/JSSESslContext.java
+++ java/org/apache/tomcat/util/net/jsse/JSSESslContext.java
@@ -1,8 +1,20 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
+
 package org.apache.tomcat.util.net.jsse;
 
 import java.security.KeyManagementException;
@@ -17,16 +29,13 @@ import javax.net.ssl.TrustManager;
 import javax.net.ssl.SSLContext;
 import org.apache.tomcat.util.net.SslContext;
 
-/**
- *
- * @author leo
- */
-public class JSSESslContext extends SslContext{
+public class JSSESslContext extends SslContext {
 
     private SSLContext context;
-    
+
     @Override
-    public void init(KeyManager[] kms, TrustManager[] tms, SecureRandom sr) throws KeyManagementException {
+    public void init(KeyManager[] kms, TrustManager[] tms, SecureRandom sr)
+            throws KeyManagementException {
         context.init(kms, tms, sr);
     }
 
@@ -51,8 +60,9 @@ public class JSSESslContext extends SslContext{
     }
 
     @Override
-    protected void initiateProtocol(String protocol) throws NoSuchAlgorithmException {
+    protected void initiateProtocol(String protocol)
+            throws NoSuchAlgorithmException {
         context = SSLContext.getInstance(protocol);
     }
-    
+
 }
